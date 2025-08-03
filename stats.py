8b6482817d4c1cfa19021c47657b5c36622c7f70
joonmo-you <joonmo.you@gmail.com>
2025-08-03 11:59:58 +0900

    print a report

--- stats.py
+++ stats.py
@@ -2,12 +2,25 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
+
 def get_chars_dict(text):
     chars = {}
-
-    for char in text.lower():
-        if char not in chars:
-            chars[char] = 0
-        chars[char] += 1
-
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
     return chars
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
